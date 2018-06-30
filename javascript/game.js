4ed7d500bfa51ab93b4272af317adc34610c4444
William <william.ma9215@gmail.com>
2018-06-29 21:22:29 -0400

    finalized working game

--- javascript/game.js
+++ javascript/game.js
@@ -1,33 +1,34 @@
-// var start;
 // var time = [1, 5, 0];
+var start;
 var Game = {
-  start: true,
+  start: null,
   hours: 1,
   minutes: 5,
   seconds: 0,
   score: 0,
-  lives: 0,
+  lives: 10,
+  round: 0,
   lettersUsed: "",
-  words: ["Apple", "Orange", "Banana"],
+  hints: ["An ____ a Day will keep the Doctor Away.", "\"_____ is the new Black\"", "Category: Fruit"],
+  words: [["fruits", "apple", "orange", "banana"],["animals", "dog", "cat", "mouse"],["colors", "red" ,"yellow", "blue"]],
   currentWord: "",
   currentDisplay: "_ ",
 
   chooseRandomWord: function () {
-    this.currentWord = this.words[Math.floor(Math.random() * 3)];
+    var temp = Math.floor(Math.random() * 3);
+    this.currentWord = this.words[temp][Math.floor(Math.random() * 3) + 1];
     console.log("Chosen Word: " + this.currentWord);
     for (var i = 0; i < this.currentWord.length-1; i++) {
       this.currentDisplay = this.currentDisplay + "_ ";
     }
     document.getElementById("Display").textContent = this.currentDisplay;
+    document.getElementById("Category").textContent = "Category: " + this.words[temp][0];
   }
 };
 
-
-
-//document.getElementById("Body").style.backgroundSize = "60px 120px";
 window.onload = function(){
-  Game.chooseRandomWord();
 
+  document.getElementById("Restart").style.display = "none";
   //Volume Adjustments
   document.getElementById("Jigsaw").volume = 0.4;
   document.getElementById("AudioEasy").volume = 0.4;
@@ -39,13 +40,15 @@ window.onload = function(){
   document.getElementById("Jigsaw").onmouseup = function () {
     setTimeout(function() {
       Game.start = false;
-    }, 23000)
+    }, 22500)
   };
 
   document.getElementById("Easy").onmouseup = function() {
     if (Game.start === false) {
       Game.start = true;
       document.getElementById("AudioEasy").play();
+      Game.lives = 1;
+      document.getElementById("Guesses").textContent = Game.lives;
       setTimeout(changeTime, 6000);
       setTimeout(startFunction, 11000);
     }
@@ -69,25 +72,80 @@ window.onload = function(){
     }
   };
 
+  //Restart Function
+  document.getElementById("Restart").onmouseup = function() {
+    Game.start = true;
+    Game.currentWord = "";
+    Game.currentDisplay = "_ ";
+    Game.lives = 10;
+    document.getElementById("Guesses").textContent = Game.lives;
+    Game.lettersUsed = "";
+    document.getElementById("Letters").textContent = Game.lettersUsed;
+    Game.score = 0;
+    document.getElementById("Words").textContent = Game.score;
+    document.getElementById("Restart").style.display = "none";
+    Game.minutes = 5;
+    Game.seconds = 0;
+    document.getElementById("Minutes").textContent = "0" + Game.minutes;
+    document.getElementById("Seconds").textContent = "0" + Game.seconds;
+    Game.chooseRandomWord();
+    start = setInterval(function () { timerSeconds(Game.seconds); }, 1000);
+  };
+
   document.onkeyup = function(event) {
     if (Game.start === true && event.keyCode >= 65 && event.keyCode <= 90 && Game.lettersUsed.includes(event.key) === false) {
       Game.lettersUsed = Game.lettersUsed + event.key + ", ";
       document.getElementById("Letters").textContent = Game.lettersUsed;
+      if (Game.currentWord.includes(event.key) === true && document.getElementById("Display").textContent.includes("_") === true) {
+        var newWord = "";
+        for (var i = 0; i < Game.currentWord.length; i++) {
+         if (event.key === Game.currentWord[i]) {
+           newWord += (event.key + " ");
+         } else if (Game.lettersUsed.includes(Game.currentWord[i]) === true) {
+           newWord += Game.currentWord[i] + " ";
+         } else {
+           newWord += "_ "
+         }
+        }
+        document.getElementById("Display").textContent = newWord;
+        if (document.getElementById("Display").textContent.includes("_") === false)
+        {
+          Game.score++;
+          document.getElementById("Words").textContent = Game.score;
+          Game.lettersUsed = "";
+          document.getElementById("Letters").textContent = Game.lettersUsed;
+          Game.currentWord = "";
+          Game.currentDisplay = "_ ";
+          Game.chooseRandomWord();
+        }
+      } else {
+        Game.lives--;
+        document.getElementById("Guesses").textContent = Game.lives;
+        if (Game.lives === 0) {
+          document.getElementById("Display").textContent = "G A M E  O V E R";
+          document.getElementById("Minutes").textContent = "00";
+          document.getElementById("Seconds").textContent = "00";
+          document.getElementById("Restart").style.display = "";
+          clearTimeout(start);
+          Game.start = null;
+        }
+      }
     }
   };
 
   function changeTime() {
     document.getElementById("Tick").play();
     document.getElementById("Hours").textContent = "00";
+    Game.hours = 0;
     document.getElementById("Minutes").textContent = "0" + 5;
   };
 
   function startFunction() {
-    setInterval(function () { timerSeconds(time[2]) }, 1000);
+    start = setInterval(function () { timerSeconds(Game.seconds); }, 1000);
     document.getElementById("DIFFICULTY").style.display = "none";
     document.getElementById("Jigsaw").style.display = "none";
     document.getElementById("Time").style.margin = "25% -100% 0px 0px";
-    Game.start = true;
+    Game.chooseRandomWord();
   };
 
   function timerSeconds(i) {
@@ -95,12 +153,16 @@ window.onload = function(){
         i--;
       } else {
         i = 59;
-        time[1]--;
-        formatTime("Minutes", time[1]);
+        Game.minutes--;
+        formatTime("Minutes", Game.minutes);
       }
-      time[2] = i;
+      Game.seconds = i;
       document.getElementById("Beep").play();
       formatTime("Seconds", i);
+      if(Game.minutes === 0 && Game.seconds === 0) {
+        clearTimeout(start);
+        console.log("GAME OVER");
+      }
   };
 
   function formatTime(element, time) {
